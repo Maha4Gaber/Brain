58cee6df07547b62493ef3f2d3368c9d6e3bc803
Maha4Gaber <77496313+Maha4Gaber@users.noreply.github.com>
2024-04-06 16:14:13 +0200

    boot

--- src/App.vue
+++ src/App.vue
@@ -28,5 +28,22 @@ body{
   --graycolor:#BBE0DD;
   --textcolor:#199A8D;
 }
-
+a.abtn{
+  padding: 5px 25px;
+  gap: 10px;
+  border-radius: 21px;
+  color: inherit;
+  text-decoration: none;
+  text-align: center;
+  display: inline-block;
+  
+}
+a.abtn1{
+  background:var(--graycolor);
+  color: black;
+}
+a.abtn2{
+  background:white;
+  color:var(--textcolor);
+}
 </style>
